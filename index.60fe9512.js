e71e794283065a653f90e4d5c14fee790c0eadc5
etrigo1 <bjorn.sjostedt@gmail.com>
2023-01-17 09:31:37 +0100

    Updates

--- index.60fe9512.js
+++
@@ -1,2 +0,0 @@
-const cookieBtns=document.querySelectorAll(".cookie-btn"),cookieBox=document.querySelector(".cookie-box"),hideCookieBoxFn=()=>cookieBox.classList.add("hidden"),hideCookies=o=>o.addEventListener("click",hideCookieBoxFn);cookieBtns.forEach(hideCookies);
-//# sourceMappingURL=index.60fe9512.js.map
